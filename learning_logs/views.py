cb7517de0b374e9b1b6973cfec7d9c1e43dae4e3
hcna0 <hahachchoi@gmail.com>
2023-02-24 00:39:58 -0500

    Use 'index.html' as home page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     """The Home Page for Learning Log."""
-    return HttpResponse("Hello, Visitor. Welcome to Learning Log.")
-    # return render(request, 'learning_logs/index.html')
+    # return HttpResponse("Hello, Visitor. Welcome to Learning Log.")
+    return render(request, 'learning_logs/index.html')
 
 def greet(request, name):
     return HttpResponse(f"Hello, {name.capitalize()}.")
