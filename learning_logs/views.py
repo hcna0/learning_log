ef961190757cd1d018709f8b369b05e637a94390
hcna0 <hahachchoi@gmail.com>
2023-02-23 22:03:39 -0500

    Home Page set to HttpResponse.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     """The Home Page for Learning Log."""
-    return render(request, 'learning_logs/index.html')
-    
+    return HttpResponse("Hello, Visitor. Welcome to Learning Log.")
+    # return render(request, 'learning_logs/index.html')
+
